e813f25df5e68d72a9771d689927f424468c2244
jignesh chaudhary <jignesh@nesscale.com>
2025-12-13 08:56:15 +0000

    added logo

--- erpnext_order_automation/erpnext_order_app/doctype/order_confirmation/order_confirmation.py
+++ erpnext_order_automation/erpnext_order_app/doctype/order_confirmation/order_confirmation.py
@@ -2,40 +2,50 @@ import requests
 import frappe
 from frappe.model.document import Document
 from frappe.model.mapper import get_mapped_doc
+import pyshorteners
 
-BITLY_TOKEN = "527220ad35b032e9ece229594da72f7f168c3811"
+# BITLY_TOKEN = "527220ad35b032e9ece229594da72f7f168c3811"
+
+
+# def shorten_url(long_url):
+#     if not BITLY_TOKEN:
+#         frappe.log_error("Missing Bitly token", "Bitly Error")
+#         return long_url
+
+#     url = "https://api-ssl.bitly.com/v4/shorten"
+#     headers = {
+#         "Authorization": f"Bearer {BITLY_TOKEN}",
+#         "Content-Type": "application/json",
+#     }
+
+#     data = {"long_url": long_url}
+
+#     try:
+#         response = requests.post(url, json=data, headers=headers)
+
+#         if response.status_code in (200, 201):
+#             return response.json().get("link", long_url)
+
+#         frappe.log_error(
+#             f"Bitly error {response.status_code}",
+#             "Bitly URL Error"
+#         )
+#         return long_url
+
+#     except Exception as e:
+#         frappe.log_error(str(e)[:140], "Bitly Exception")
+#         return long_url
+
 
 
 def shorten_url(long_url):
-    if not BITLY_TOKEN:
-        frappe.log_error("Missing Bitly token", "Bitly Error")
-        return long_url
-
-    url = "https://api-ssl.bitly.com/v4/shorten"
-    headers = {
-        "Authorization": f"Bearer {BITLY_TOKEN}",
-        "Content-Type": "application/json",
-    }
-
-    data = {"long_url": long_url}
-
     try:
-        response = requests.post(url, json=data, headers=headers)
-
-        if response.status_code in (200, 201):
-            return response.json().get("link", long_url)
-
-        frappe.log_error(
-            f"Bitly error {response.status_code}",
-            "Bitly URL Error"
-        )
-        return long_url
-
+        shortener = pyshorteners.Shortener()
+        return shortener.tinyurl.short(long_url)
     except Exception as e:
-        frappe.log_error(str(e)[:140], "Bitly Exception")
+        frappe.log_error(str(e)[:140], "Shorten URL Error")
         return long_url
-
-
+    
 class OrderConfirmation(Document):
     pass
 
